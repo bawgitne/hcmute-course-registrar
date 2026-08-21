a013cd6b83ca73659715df8ce8062f57e4727277
Developer <dev@example.com>
2026-08-21 10:32:31 +0700

    Fix NPM_CONFIG_PRODUCTION=false and --include=dev for Linux native bindings on Railway

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-npm --prefix web-app install
-npm --prefix web-app run build
+cd web-app && NPM_CONFIG_PRODUCTION=false npm install --include=dev && npm run build
 npx -y serve -s web-app/dist -l ${PORT:-8080}
